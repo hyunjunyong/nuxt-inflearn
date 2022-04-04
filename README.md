ef9df62c6a9cfafa269e07b034c452d65064e44f
현준용 <hjy4649@cocen.com>
2022-04-04 14:22:37 +0900

    pages 실행

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 4/1 캡틴 판교님의 인프런 nuxt 수강 시작
 4/2 서버사이드 렌더링이란? CSR과 SSR에 차이점
 4/3 4/2까지의 블로그 글 작성
+4/4 nuxt.js install 및 프로젝트 폴더 구조에 대해
 
 1. https://hjycoinfe.tistory.com/5 (Nuxt.js 개념)
 2. https://hjycoinfe.tistory.com/6 (CSR,SSR)
---
+++ pages/main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="name">
+    <Nuxt />
+    <Nuxt></Nuxt>
+    <!-- <router-view></router-view>에 효과와 같음 url을 이동할떄마다 nuxt태그 있는 페이지가 다시그려짐-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "name",
+
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style lang="scss" scoped></style>
